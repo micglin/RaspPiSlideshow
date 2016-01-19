2e4e9c970408e6305853115cfeb02d276ae71c47
bmallen-xs <bmallen.xs@gmail.com>
2016-01-19 04:56:48 +0000

    Testing.

--- install.sh
+++ install.sh
@@ -76,6 +76,7 @@ server {
         }
 
         location ~ \.php$ {
+                try_files $uri =404;
                 fastcgi_split_path_info ^(.+\.php)(/.+)$;
                 fastcgi_pass unix:/var/run/php5-fpm.sock;
                 fastcgi_index index.php;
